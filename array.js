23abdc89ee6bdfbe9a4462ffd06d2511aee80492
h12chan <112553752+h12chan@users.noreply.github.com>
2022-09-03 19:36:21 -0400

    Create array.js

---
+++ array.js
@@ -0,0 +1,14 @@
+train = [
+  { colour: green,
+   type: passenger},
+  { colour: red,
+   type: passenger},
+  { colour: yellow,
+   type: passenger},
+  { colour: brown,
+   type: cargo},
+  { colour: black,
+   type: cargo},
+  { colour: blue,
+   type: engine}
+  ]
