95a146ade64f0cdf73da667f0d9a37a1ff79054f
ep05asdw <89652397+ep05asdw@users.noreply.github.com>
2022-09-05 15:01:03 -0400

    Fixed Some Errors

--- array.js
+++ array.js
@@ -1,16 +1,16 @@
-train = [
-  { colour: green,
-   type: passenger},
-  { colour: red,
-   type: passenger},
-  { colour: yellow,
-   type: passenger},
-  { colour: brown,
-   type: cargo},
-  { colour: black,
-   type: cargo},
-  { colour: blue,
-   type: engine}
+let train = [
+  { 'colour': 'green',
+   'type': 'passenger'},
+  { 'colour': 'red',
+   'type': 'passenger'},
+  { 'colour': 'yellow',
+   'type': 'passenger'},
+  { 'colour': 'brown',
+   'type': 'cargo'},
+  { 'colour': 'black',
+   'type': 'cargo'},
+  { 'colour': 'blue',
+   'type': 'engine'}
   ]
 
 export default train;
--- for-each.js
+++ for-each.js
@@ -2,7 +2,7 @@ import train from './array.js';
 
 // for-each loop over array of elements in JavaScript
 function getElements(arr) {
-  a.arr.forEach((a) => {
+  arr.forEach((a) => {
     console.log(a);
   });
 }
