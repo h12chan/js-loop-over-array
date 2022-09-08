0bbc921a54e77cf245d10d4f5c5d711547477028
h12chan <112553752+h12chan@users.noreply.github.com>
2022-09-08 11:32:02 -0400

    Update for-in.js

--- for-in.js
+++ for-in.js
@@ -1,3 +1,9 @@
-for (t in train) {
-  console.log(train[t])
+import train from './array.js';
+
+function getElementsForIn(arr) {
+  for (a in arr) {
+    console.log(arr[a])
+  }  
 }
+
+export default getElementsForIn;
