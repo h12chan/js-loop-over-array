bf73f798daec7bdd8acc79c54ce00c74706d15d5
h12chan <112553752+h12chan@users.noreply.github.com>
2022-09-08 11:33:51 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 import getElements from './for-each.js';
 import train from './array.js';
+import getElementsForIn from './for-in.js';
 
+console.log("For-Each loop");
 getElements(train);
+console.log("For-In Loop");
+getElementsForIn(train);
