19928e77a90a6af32e78c8dc8a3e745792c26167
ep05asdw <89652397+ep05asdw@users.noreply.github.com>
2022-09-09 17:09:54 -0400

    Add for-of.js

---
+++ for-of.js
@@ -0,0 +1,9 @@
+import train from './array.js';
+
+function getElementsForOf(arr) {
+  for (let a of arr) {
+    console.log(a)
+  }  
+}
+
+export default getElementsForOf;
--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 import getElements from './for-each.js';
 import train from './array.js';
 import getElementsForIn from './for-in.js';
+import getElementsForOf from './for-of.js';
 
 console.log("For-Each loop");
 getElements(train);
 console.log("For-In Loop");
 getElementsForIn(train);
+console.log("For-Of loop");
+getElementsForOf(train);
